df77c833e8665c022be8884184eab28bc8a775c3
chelseyrhianne <87962835+chelseyrhianne@users.noreply.github.com>
2021-10-13 09:14:10 +1300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img
+        src={logo}
+        className="App-logo img-fluid"
+        alt="logo of a girl reading on a pile of books"
+      />
+    </div>
+  );
+}
